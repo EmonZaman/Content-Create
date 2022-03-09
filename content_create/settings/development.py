d9e6886a9d9738324e20ec29edfb779eb407501d
EmonZaman <aagontukemon@gmail.com>
2022-03-09 21:59:33 +0600

    staticfiles updated

--- content_create/settings/development.py
+++ content_create/settings/development.py
@@ -1,10 +1,11 @@
 import os
 import django_heroku
+
 from .defaults import *
+
 DEBUG = False
 
 ALLOWED_HOSTS = ['content-create-testing.herokuapp.com', '127.0.0.1']
-
 
 DATABASES = {
     'default': {
@@ -16,15 +17,22 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
-STATICFILES_DIRS = [BASE_DIR / 'static_local']
 django_heroku.settings(locals())
 # DEBUG TOOLBAR
 INTERNAL_IPS = ['127.0.0.1']
