14d6c1f79944442454b0bb01df576273c8e7a87a
EmonZaman <aagontukemon@gmail.com>
2022-03-14 18:12:55 +0600

    user model extends with extra fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -11,7 +11,12 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['id', 'username', 'email', 'first_name', 'last_name', 'country', 'is_active', 'is_staff']
     fieldsets = [
         [None, {'fields': ['username', 'password']}],
-        [_('Personal info'), {'fields': ['first_name', 'last_name', 'email']}],
+        [_('Personal info'), {'fields': ['first_name', 'last_name', 'email','country','profile_pic','secondary_email','phone','content_choice','gender','facebook','linked_in',
+                                         'full_name',
+                                         'age',
+                                         'language',
+
+                                         ]}],
         [_('Permissions'), {
             'fields': ['is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'],
         }],
