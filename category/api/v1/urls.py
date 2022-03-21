301c5cd4a60f8ffca6da4fbcabbce9d9dd81dbee
EmonZaman <aagontukemon@gmail.com>
2022-03-21 21:04:33 +0600

    url path name updated items to categories and video_category to videos_by_category

--- category/api/v1/urls.py
+++ category/api/v1/urls.py
@@ -3,9 +3,9 @@ from category.api.v1.views import CategoryDetailAPIView, CategoryListApiView, Vi
     category_content
 
 urlpatterns = [
-    path('items/', CategoryListApiView.as_view(), name="category_list"),
-    path('items/<int:pk>/', CategoryDetailAPIView.as_view(), name="category_list_individual"),
+    path('categories/', CategoryListApiView.as_view(), name="category_list"),
+    path('categories/<int:pk>/', CategoryDetailAPIView.as_view(), name="category_list_individual"),
     path('video/', VideoListApiView.as_view(), name="video_list"),
     path('video/<int:pk>/', VideoDetailAPIView.as_view(), name="video_list_individual"),
-    path('video_category/', category_content.as_view(), name="video_catgory")
+    path('videos_by_category/', category_content.as_view(), name="video_category")
 ]
