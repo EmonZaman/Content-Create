e304ee87e5b1ea350df4c7bb4ed5310d2093995b
EmonZaman <aagontukemon@gmail.com>
2022-03-23 21:04:13 +0600

    user model is_pro and and pro expiry date added and differnets types to filter total user, prousers etc added and and last seven dayes users and subscriber added

--- category/api/v1/urls.py
+++ category/api/v1/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from category.api.v1.views import CategoryDetailAPIView, CategoryListApiView, VideoListApiView, VideoDetailAPIView, \
-    category_content
+    category_content, LastSevenDaysUserListAPIView, LastSevenDaySubscriberListAPIView, UserAndSubscriberCountAPIView
 
 urlpatterns = [
     path('categories/', CategoryListApiView.as_view(), name="category_list"),
     path('categories/<int:pk>/', CategoryDetailAPIView.as_view(), name="category_list_individual"),
     path('video/', VideoListApiView.as_view(), name="video_list"),
     path('video/<int:pk>/', VideoDetailAPIView.as_view(), name="video_list_individual"),
-    path('videos_by_category/', category_content.as_view(), name="video_category")
+    path('videos_by_category/', category_content.as_view(), name="video_category"),
+
+    path('last_seven_days_user_list/', LastSevenDaysUserListAPIView.as_view(), name="Last seven Days user list"),
+    path('last_seven_subscriber_list/', LastSevenDaySubscriberListAPIView.as_view(), name="Last seven Days Subscriber list"),
+    path('count/', UserAndSubscriberCountAPIView.as_view(),
+         name="Detail Count"),
+
 ]
--- category/api/v1/views.py
+++ category/api/v1/views.py
@@ -1,9 +1,12 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView, GenericAPIView
 from rest_framework.parsers import FormParser, MultiPartParser
-
+from rest_framework.response import Response
 from accounts.api.v1.serializers import UserSerializer
+from accounts.models import User
 from category.api.v1.serializers import CategorySerializer, VideoSerializer
 from category.models import Category, Video
+
+from datetime import datetime, timedelta
 
 
 class CategoryListApiView(ListCreateAPIView):
@@ -34,3 +37,56 @@ class category_content(ListAPIView):
         category = self.request.query_params.get('category', None)
 
         return Video.objects.filter(category__name=category)
+
+
+class LastSevenDaysUserListAPIView(ListCreateAPIView):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        queryset = User.objects.filter(date_joined__gte=datetime.now() - timedelta(days=7))
+        return queryset
+class LastSevenDaySubscriberListAPIView(ListCreateAPIView):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        queryset = User.objects.filter(date_joined__gte=datetime.now() - timedelta(days=7), is_pro=True)
+        return queryset
+class UserAndSubscriberCountAPIView(GenericAPIView):
+    serializer_class = UserSerializer
+
+    def get(self, request, *args, **kwargs):
+        response = {}
+        # total_user= self.request.query_params.get('User', None)
+        total_pro_user = self.request.query_params.get('total_pro_user', None)
+        pro_user_seven = self.request.query_params.get('pro_user_seven', None)
+        pro_user_today = self.request.query_params.get('pro_user_today', None)
+        users_last_seven_days= self.request.query_params.get('users_last_seven_days', None)
+        total_users = self.request.query_params.get('total_users', None)
+        users_today = self.request.query_params.get('users_today', None)
+
+        if total_pro_user=="true":
+            pro_user_count = User.objects.filter(is_pro=True).count()
+            response['Total_pro_users']= pro_user_count
+        if pro_user_seven=="true":
+            pro_user_count = User.objects.filter(date_joined__gte=datetime.now() - timedelta(days=7), is_pro=True).count()
+            response['pro_users_last_seven_days']= pro_user_count
+        if pro_user_today=="true":
+            pro_user_count = User.objects.filter(date_joined__gte=datetime.now() - timedelta(days=1), is_pro=True).count()
+            response['pro_users_today']= pro_user_count
+        if users_last_seven_days == "true":
+            pro_user_count = User.objects.filter(date_joined__gte=datetime.now() - timedelta(days=7)).count()
+            response['users_last_seven_days'] = pro_user_count
+        if total_users == "true":
+            pro_user_count = User.objects.count()
+            response['total_users'] = pro_user_count
+
+        if users_today == "true":
+            pro_user_count = User.objects.filter(date_joined__gte=datetime.now() - timedelta(days=1)).count()
+            response['users_today'] = pro_user_count
+
+
+
+        return Response(response)
+
+
+
