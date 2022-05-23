9a434724818a68110afca45387fce45b22199a5a
EmonZaman <aagontukemon@gmail.com>
2022-05-23 22:35:43 +0600

    aws_default_acl_ public added

--- content_create/settings/defaults.py
+++ content_create/settings/defaults.py
@@ -228,9 +228,11 @@ AWS_S3_ACCESS_KEY_ID = env.str("AWS_S3_ACCESS_KEY_ID")
 AWS_S3_SECRET_ACCESS_KEY = env.str("AWS_S3_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = env.str("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_ENDPOINT_URL="https://nyc3.digitaloceanspaces.com"
+AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_OBJECT_PARAMETERS={
     "CacheControl": "max-age=86400",
 }
+
 AWS_LOCATION = "https://serenity-digital-storage.nyc3.digitaloceanspaces.com/"
 AWS_QUERYSTRING_AUTH = False
 
